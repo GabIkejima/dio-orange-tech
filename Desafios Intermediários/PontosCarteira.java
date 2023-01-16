03fc564bf61a1af1f9893f35345ba331fb753533
GabIkejima <118475573+GabIkejima@users.noreply.github.com>
2023-01-16 18:33:17 -0300

    Update PontosCarteira.java

--- Desafios Intermediários/PontosCarteira.java
+++ Desafios Intermediários/PontosCarteira.java
@@ -16,7 +16,7 @@ public class PontosCarteira {
             if(velocidade > 80) multas++;
 
             //Verifica se o motorista irá receber pontos na carteira
-            if(multas>=3){
+            if(multas >= 3){
                 System.out.println(multas + " multas. Levou pontos na carteira");
             }else {
                 System.out.println(multas + " multas. Nao levou pontos na carteira");
