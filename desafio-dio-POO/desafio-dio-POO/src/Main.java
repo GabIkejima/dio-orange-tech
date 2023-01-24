b7f560fc673975d71c577a9e86d93d18ec3a9848
GabIkejima <gabrielhigaikejima@gmail.com>
2023-01-24 15:23:29 -0300

    Adicionado class Curso e Mentoria

--- desafio-dio-POO/desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/desafio-dio-POO/src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Curso de Orientação a objetos Java");
+        curso1.setCargaHoraria(20);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Python");
+        curso2.setDescricao("Curso de APIS Flask Python");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria voltada para a mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
